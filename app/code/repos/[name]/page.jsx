cc90a187da22bdc3e3058071cdb3d14b12ce26fd
JunjPan <elisepjj@hotmail.com>
2023-05-29 15:38:44 -0700

    create repoDirs component

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,13 +1,17 @@
 import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
 import Link from "next/link";
 
-const RepoPage = ({params:{name}}) => {
-
+const RepoPage = ({ params: { name } }) => {
   return (
     <div className="card">
-        <Link href="/code/repos" className="btn btn-back">Back to Repositories</Link><Repo name={name} /></div>
-  ) 
- 
-}
+      <Link href="/code/repos" className="btn btn-back">
+        Back to Repositories
+      </Link>
+      <Repo name={name} />
+      <RepoDirs name={name} />
+    </div>
+  );
+};
 
-export default RepoPage
+export default RepoPage;
