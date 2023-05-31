98ac6e5cd91476319330db93ce8f8edac2252612
JunjPan <elisepjj@hotmail.com>
2023-05-30 20:27:51 -0700

    getting body data

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import courses from './data.json';
+import {v4 as uuidv4} from 'uuid';
 
 //it uses to use handler function
 /**
@@ -14,4 +15,18 @@ import courses from './data.json';
  */
 export async function GET(request){
     return NextResponse.json(courses)
+}
+
+export async function POST(request){
+    const {title,description,level,link}=await request.json();
+
+    const newCourse={
+        id:uuidv4(),
+        title,
+        description,
+        level,
+        link
+    }
+    courses.push(newCourse);
+    return NextResponse.json({courses})
 }
